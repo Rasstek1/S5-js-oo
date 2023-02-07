6d2f586404143b974a5cdd639d661e92392c262a
Rasstek1 <2295093@cegepoutaouais.qc.ca>
2023-02-06 22:35:18 -0500

    Lundi soir 22h30

--- js/script.js
+++ js/script.js
@@ -1,17 +1,88 @@
-function updateSoustotal(champQuantite, soustotalPoire) {
+/*function updateSoustotal(champQuantite, soustotalPoire) {
     let quantite = parseInt(champQuantite.value);
-    let prix = parseFloat(soustotalPoire.dataset.prix);
-    soustotalPoire.value = (quantite * prix).toFixed(2);
+    let prix;
+     prixPeches.value = (quantite * prix).toFixed(2);
+
+    if (fruit === 'Peches'){
+        prix = 10.00;// peut etre changer dans le html pour un id pour lier les futurs changement
+        idPrix = 'prixPeches';
+    } 
+
+    //prend l'ancien prix  et l'ajoute au prix pour creer le nouveau prix
+    let ancienPrix = document.getElementById(idPrix).innerText;
+    let nouveauPrix = +ancienPrix + +prix;
+    document.getElementById(idPrix).innerText = nouveauPrix;//getElementById (selector) remplace prixPommes par nouveauprix
+
   }
 
 
- function checkSubtotal() {
+ function checkSoustotal() {
     let total = 0;
-    let subtotalElements = document.querySelectorAll('.totalFruit');
-    subtotalElements.forEach(function(element) {
+    let nbFruit = document.querySelectorAll('totalFruit');
+    nbFruit.forEach(function(element) {
       total += parseFloat(element.value);
     });
     if (total < 20) {
-      alert("Le montant minimum du panier est de 20 $. Veuillez ajouter des articles à votre panier pour atteindre");
+      alert("Montant minimum 20 $. Veuillez ajouter des articles à votre panier pour atteindre 20 piastres!!!");
     }
+}*/
+
+
+
+
+function Ajuster(fruit) {
+    let sousTotalPeches = document.getElementById("sousTotalPeches");
+    let sousTotalPoires = document.getElementById("sousTotalPoires");
+    let sousTotalPommes = document.getElementById("sousTotalPommes");
+    switch (fruit) {
+        case "Peches":
+            sousTotalPeches.innerHTML = parseInt(document.getElementById("prixPeches").innerText) * document.getElementById("nbPeches").value;
+            break;
+        case "Poires":
+            sousTotalPoires.innerHTML = parseInt(document.getElementById("prixPoires").innerText) * document.getElementById("nbPoires").value;
+            break;
+        case "Pommes":
+            sousTotalPommes.innerHTML =  parseInt(document.getElementById("prixPommes").innerText) * document.getElementById("nbPommes").value;
+            break;
+    }
+    document.getElementById("total").innerText  = +sousTotalPeches.innerHTML + +sousTotalPommes.innerHTML + +sousTotalPoires.innerHTML;
+
+
 }
+
+
+function Valider(){
+    let totalFruits = +document.getElementById("nbPeches").value + +document.getElementById("nbPoires").value + +document.getElementById("nbPommes").value
+    document.getElementById("errorMax").classList.add("invisible")
+    document.getElementById("errorValeur").classList.add("invisible")
+
+    if(totalFruits>24){
+
+    document.getElementById("errorMax")
+        document.getElementById("errorMax").classList.remove("invisible")
+        return false;
+
+    }if ( document.getElementById("total").innerText < 20){
+        document.getElementById("errorValeur").classList.remove("invisible")
+        return false;
+    }
+
+    return true;
+}
+
+
+
+
+
+   
+  
+
+/*function total() {
+    let total = 0;
+    let nbFruit = document.querySelectorAll('totalFruit');
+    nbFruit.forEach(function(element) {
+      total += parseFloat(element.value);
+    });
+    if (total < 20) {
+      alert("Montant minimum 20 $. Veuillez ajouter des articles à votre panier pour atteindre 20 piastres!!!");
+    }*/
